ec87c63487dc66836172d4ba6c5dc2efb702f465
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-18 12:07:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def home():
   return render_template("index.html")
 
+@app.route("/api/test")
+def test():
+  return "test"
+
 if __name__ == "__main__":
   app.run(debug=True)
