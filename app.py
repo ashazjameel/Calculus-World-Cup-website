1428ea22849e1539c03572552004a58a9f002115
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-22 21:19:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 from authlib.integrations.flask_client import OAuth
+import os
 
 app = Flask(__name__)
 oauth = OAuth(app)
 google = oauth.register(
     name="google",
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
+    client_id=os.environ.get("CLIENT_ID"),
+    client_secret=os.environ.get("CLIENT_SECRET"),
     server_metadata_uri="http://accounts.google.com/.well-known/openid-configuration",
     client_kwards = {"scope":"openid profile email"}
 )
