be16ac86453cc1883f8bb6f77c6183790bf42b76
Ashaz Jameel <161173725+ashazjameel@users.noreply.github.com>
2026-09-24 16:25:28 +0100

    deleted temp code

--- static/script.js
+++ static/script.js
@@ -3,8 +3,3 @@ function lightDark() {
   document.getElementById("lightButton").classList.toggle("light-button");
   document.getElementById("lightButton").classList.toggle("dark-button");
 }
-
-function fetchData() {
-  fetch("/api/test")
-    .then(response => alert(response));
-}
